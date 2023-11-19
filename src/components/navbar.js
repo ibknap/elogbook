9bf040cc29ac1ba5bbd8c76cfbc64ca0c4329e0a
ibukunoluwanap <promisenap2001@gmail.com>
2023-11-19 16:35:31 +0200

    added auth

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,186 @@
 import Link from "next/link";
 import { useMediaQuery } from "@chakra-ui/react";
 import { Menu } from "iconsax-react";
+import { useAuth } from "@/firebase/fire_auth_context";
+import { doc, onSnapshot } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { useState, useEffect } from "react";
+import { db } from "@/firebase/fire_config";
 
 export default function Navbar() {
+  const [user, setUser] = useState(null);
+  const { loading, authUser, logOut } = useAuth();
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+  useEffect(() => {
+    if (authUser) {
+      const userRef = doc(db, "users", authUser.email);
+      const unsubscribe = onSnapshot(userRef, (doc) => {
+        if (doc.exists()) setUser(doc.data());
+        else toast.error("User not found");
+      });
+      return () => unsubscribe();
+    }
+  }, [authUser]);
+
+  if (user && user.isStudent && authUser) {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light p-0 bg-primary">
+        <div className="container-fluid ps-0">
+          <Link
+            className="navbar-brand bg-white d-flex pe-2 align-items-center"
+            href="/"
+          >
+            <img src="/logo.png" width={isMobile ? 60 : 100} alt="logo" />
+
+            {!isMobile ? (
+              <div>
+                <h5 className="text-primary">
+                  MODIBBO ADAMA
+                  <br />
+                  UNIVERSITY, YOLA
+                </h5>
+                <small className="fw-light">Established in 1981</small>
+              </div>
+            ) : (
+              <div>
+                <h6 className="text-primary">
+                  MODIBBO ADAMA
+                  <br />
+                  UNIVERSITY, YOLA
+                </h6>
+                <small className="fw-light">Established in 1981</small>
+              </div>
+            )}
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <Menu size={28} className="text-white border-light" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link text-white" href="/student_bio">
+                  Student Bio
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" href="/student/e_logbook">
+                  E-LogBook
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link text-white"
+                  href="/student/org_details"
+                >
+                  Org Details
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link text-white" href="/student/itf_form">
+                  ITF Form
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <button className="nav-link text-danger" onClick={logOut}>
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+  if (user && user.isSupervisor && authUser) {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light p-0 bg-primary">
+        <div className="container-fluid ps-0">
+          <Link
+            className="navbar-brand bg-white d-flex pe-2 align-items-center"
+            href="/"
+          >
+            <img src="/logo.png" width={isMobile ? 60 : 100} alt="logo" />
+
+            {!isMobile ? (
+              <div>
+                <h5 className="text-primary">
+                  MODIBBO ADAMA
+                  <br />
+                  UNIVERSITY, YOLA
+                </h5>
+                <small className="fw-light">Established in 1981</small>
+              </div>
+            ) : (
+              <div>
+                <h6 className="text-primary">
+                  MODIBBO ADAMA
+                  <br />
+                  UNIVERSITY, YOLA
+                </h6>
+                <small className="fw-light">Established in 1981</small>
+              </div>
+            )}
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <Menu size={28} className="text-white border-light" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link text-white"
+                  href="/supervisor/profile"
+                >
+                  Profile
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  className="nav-link text-white"
+                  href="/supervisor/manage_students"
+                >
+                  Manage Students
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <button className="nav-link text-danger" onClick={logOut}>
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 
   return (
     <nav className="navbar navbar-expand-md navbar-light p-0 bg-primary">
@@ -50,37 +227,31 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <Link className="nav-link text-white" href="/student_dashboard">
+              <Link className="nav-link text-white" href="/signin">
                 Student Dashboard
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link
-                className="nav-link text-white"
-                href="/industry_based_supervisor"
-              >
+              <Link className="nav-link text-white" href="/signin">
                 Industry-Based Supervisor
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link
-                className="nav-link text-white"
-                href="/institution_supervisor"
-              >
+              <Link className="nav-link text-white" href="/signin">
                 Institution Supervisor
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link text-white" href="/itf_dashboard">
+              <Link className="nav-link text-white" href="/signin">
                 ITF Dashboard
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link text-white" href="/admin">
+              <Link className="nav-link text-white" href="/signin">
                 Admin
               </Link>
             </li>
---
+++ src/pages/signin.js
@@ -0,0 +1,157 @@
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { useAuth } from "@/firebase/fire_auth_context";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { Information } from "iconsax-react";
+import { db } from "@/firebase/fire_config";
+import { doc, getDoc } from "firebase/firestore";
+
+export default function Signin() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading, authUser, signIn, logOut } = useAuth();
+  const router = useRouter();
+
+  const onSignin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await signIn(email, password)
+      .then((_) => {
+        getDoc(doc(db, "users", email))
+          .then(async (doc) => {
+            if (doc.exists()) {
+              const user = doc.data();
+              if (user.isAdmin) {
+                Cookies.set("ElogBookSignIn", true, {
+                  expires: 14,
+                });
+                toast.dark("Welcome admin");
+                router.push("/");
+              } else if (user.isSupervisor) {
+                Cookies.set("ElogBookSignIn", true, {
+                  expires: 14,
+                });
+                toast.dark("Welcome supervisor");
+                router.push("/");
+              } else if (user.isStudent) {
+                Cookies.set("ElogBookSignIn", true, {
+                  expires: 14,
+                });
+                toast.dark("Welcome student");
+                router.push("/");
+              } else {
+                toast.dark("User has no access", { className: "text-danger" });
+                logOut();
+              }
+            } else
+              toast.dark("User does not exist", { className: "text-danger" });
+          })
+          .catch((error) => {
+            toast.dark(`Error: ${error.message}`, { className: "text-danger" });
+          })
+          .finally((_) => setIsLoading(false));
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-login-credentials") {
+          toast.dark("Invalid sign in credentials", {
+            className: "text-danger",
+          });
+        } else if (error.code === "auth/too-many-requests") {
+          toast.dark("Too many requests", {
+            className: "text-danger",
+          });
+        } else {
+          toast.dark(`Error: ${error.message}`, {
+            className: "text-danger",
+          });
+        }
+      })
+      .finally((_) => setIsLoading(false));
+  };
+
+  if (authUser && Cookies.get("ElogBookSignIn")) {
+    return (
+      <div className="d-flex align-items-center vh-100">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-sm-6 text-primary text-center">
+              <Information size={200} />
+              <p>You logged in already.</p>
+
+              <hr />
+
+              <div className="d-flex justify-content-center">
+                <Link
+                  href="/"
+                  className="btn btn-dark bg-primary border-0 w-50"
+                >
+                  Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="d-flex vh-100 text-center">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto" />
+
+        <div className="row justify-content-center">
+          <Link href="/">
+            <img src="/logo.png" width={150} alt="logo" />
+          </Link>
+
+          <h4 className="mt-3">E-LogBook Sign In</h4>
+
+          <form className="col-md-4 text-start mt-5" onSubmit={onSignin}>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="emailAddress">
+                Email address
+              </label>
+              <input
+                type="email"
+                required
+                className="form-control cus-form-control"
+                id="emailAddress"
+                placeholder="name@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                required
+                className="form-control cus-form-control"
+                id="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="btn btn-dark bg-primary border-0 w-100"
+            >
+              {isLoading ? "Signing..." : "Sign In"}
+            </button>
+          </form>
+        </div>
+
+        <footer className="mt-auto text-muted" />
+      </div>
+    </div>
+  );
+}
